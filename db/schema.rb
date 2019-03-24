e3fd165a9c60080577f10f1b4d2a7fe1b8937400
Cinthya Flores <cinthya@MacBook-Pro-de-Cinthya.local>
2019-03-23 20:28:53 -0600

    adds styles and responsive tools

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_042739) do
+ActiveRecord::Schema.define(version: 2019_03_07_042719) do
 
   create_table "comprehension_options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
