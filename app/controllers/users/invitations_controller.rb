cdda803bddb82c3d5da5aaa6fc8fc75276fa9bb0
cinthyaflores <lizeth.clfc@gmail.com>
2019-09-27 16:20:49 -0500

    Users name is updated when accept the invitation and edit they data

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,8 +1,13 @@
 class Users::InvitationsController < Devise::InvitationsController
   before_action :configure_permitted_parameters, only: %i[create]
+  before_action :update_params, only: :update
 
   private
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:invite, keys: [:user_type])
+    end
+
+    def update_params
+      devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
     end
 end
